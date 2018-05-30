a46dae25b7bde02515378ba5f6928ed671a7b66d
sooyoung_kil <sooyoung_kil@tmax.co.kr>
2018-05-30 15:40:07 +0900

    에러수정

--- Exercise/src/Printer/FindA7.java
+++ Exercise/src/Printer/FindA7.java
@@ -2,7 +2,7 @@ package Printer;
 
 public class FindA7 {
 	public void findA7(String inputLine){
-		for(int i=0; i<inputLine.length()-1;i++){
+		for(int i=0; i<inputLine.length();i++){
 			if(inputLine.charAt(i)=='a'||inputLine.charAt(i)=='7'){
 				System.out.println("Error : there is "+inputLine.charAt(i));
 			}
